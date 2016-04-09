f4ca0caf93ed6e09b79cb1c87d4d7179de32011a
Simo-Pekka Kerkelä <simopekka1990@gmail.com>
2016-04-09 14:54:56 +0300

    enhancements to both client and server

--- client.js
+++ client.js
@@ -11,23 +11,29 @@ const client = dgram.createSocket('udp4');
 var lastCommFromServer = process.hrtime();
 var keepUpdating = true;
 var myPos =  {};
+var myPort = 0;
+var worldState = {};
 
 function send(msg) {
   const message = new Buffer(msg);
   client.send(message, 0, message.length, PORT, HOST, (err, bytes) => {
     if(err) throw err;
-    console.log('UDP message sent to ' + HOST + ':' + PORT + ':' + msg);
   });
 }
 
 client.on('message', (message, remote) => {
-  var strMessage = message.toString();
-  if(strMessage === 'ping') {
+  var command = message.toString().split('::');
+  if(command[0] === 'ping') {
     lastCommFromServer = process.hrtime();
     send('pong');
-  } else {
-    myPos = JSON.parse(strMessage);
-    console.log(myPos);
+  } else if(command[0] ==='port-is') {
+    console.log('received port:', command[1]);
+    myPort = parseInt(command[1]);
+    send('set-name::LOLBAL'+Math.random());
+  } else if(command[0] === 'set-pos') {
+    myPos = JSON.parse(command[1]);
+  } else if(command[0] === 'data') {
+    worldState = JSON.parse(command[1]);
   }
 });
 
@@ -35,8 +41,13 @@ updater.takeWhile(() => keepUpdating).onValue(() => {
   var curTime = process.hrtime();
   if(curTime[0] - lastCommFromServer[0] > 1) {
     keepUpdating = false;
+    console.log('server disconnected');
     client.close();
   }
 });
+
+B.repeatedly(1000,['state']).takeWhile(() => keepUpdating).onValue(() => {
+  console.log('known state of the game', worldState);
+})
 
 send('give-port');
--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express();
 const clientPort = 33334;
 const knownClients = {};
 
+const pongs = B.repeatedly(1000/3, ['update']);
 const updates = B.repeatedly(16, ['update']);
 
 server.on('listening', () => {
@@ -17,18 +18,27 @@ server.on('listening', () => {
 });
 
 server.on('message', (message, remote) => {
-  if(!knownClients[remote.port]) {
-    console.log(remote.port + ' connected');
+  var remotePort = remote.port
+  if(!knownClients[remotePort]) {
+    console.log(remotePort+ ' connected');
   }
-  switch (message.toString()) {
+  var command = message.toString().split('::');
+  switch (command[0]) {
     case 'give-port':
-      knownClients[remote.port] = {
+      knownClients[remotePort] = {
         lastComm: process.hrtime(),
         position: {x: 0, y: 0}
       };
+      var portMessage = new Buffer('port-is::'+remotePort);
+      server.send(portMessage,0, portMessage.length, parseInt(remotePort), 'localhost');
       break;
     case 'pong':
-      knownClients[remote.port].lastComm = process.hrtime()
+      knownClients[remotePort].lastComm = process.hrtime()
+      break;
+    case 'set-name':
+      console.log('setting name of', remotePort,'to', command[1]);
+      knownClients[remotePort].name = command[1];
+      break;
     default:
   }
 });
@@ -39,20 +49,29 @@ function tooLong(clientLastComm, currentTime) {
 
 server.bind(PORT, HOST);
 
+pongs.onValue(function () {
+  const knownKeys = Object.keys(knownClients);
+  const curTime = process.hrtime();
+  knownKeys.forEach(k => {
+    var message = new Buffer('ping');
+    server.send(message, 0, message.length, parseInt(k), 'localhost');
+    if(tooLong(knownClients[k].lastComm, curTime)) {
+      console.log(k,'disconnected');
+      delete knownClients[k];
+    }
+  })
+});
+
 updates.onValue(function () {
   const knownKeys = Object.keys(knownClients);
   const curTime = process.hrtime();
   knownKeys.forEach(k => {
     knownClients[k].position.x += 0.01;
     knownClients[k].position.y += 0.01;
-    var message = new Buffer('ping');
-    var pos = new Buffer(JSON.stringify(knownClients[k].position));
-    server.send(message, 0, message.length, parseInt(k), 'localhost');
+    var pos = new Buffer('set-pos::'+JSON.stringify(knownClients[k].position));
     server.send(pos, 0, pos.length, parseInt(k), 'localhost');
-    if(tooLong(knownClients[k].lastComm, curTime)) {
-      console.log(k,'disconnected');
-      delete knownClients[k];
-    }
+    var data = new Buffer('data::'+JSON.stringify(knownClients));
+    server.send(data, 0, data.length, parseInt(k), 'localhost');
   })
 });
 
