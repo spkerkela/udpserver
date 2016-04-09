b0fad29b9ad62ce1faa45756bb874e2f9fad913a
Simo-Pekka Kerkelä <simopekka1990@gmail.com>
2016-04-09 03:38:58 +0300

    initial client and server :)

--- client.js
+++ client.js
@@ -2,18 +2,41 @@ const B = require('baconjs');
 const PORT = 33333;
 const HOST = 'localhost';
 
-const sender = B.repeatedly(1000, ['give-port']);
+const updater = B.repeatedly(16, ['update'])
 
 const dgram = require('dgram');
 
 const client = dgram.createSocket('udp4');
 
+var lastCommFromServer = process.hrtime();
+var keepUpdating = true;
+var myPos =  {};
+
 function send(msg) {
   const message = new Buffer(msg);
   client.send(message, 0, message.length, PORT, HOST, (err, bytes) => {
     if(err) throw err;
-    console.log('UDP message sent to ' + HOST + ':' + PORT);
+    console.log('UDP message sent to ' + HOST + ':' + PORT + ':' + msg);
   });
 }
 
-sender.onValue(send);
+client.on('message', (message, remote) => {
+  var strMessage = message.toString();
+  if(strMessage === 'ping') {
+    lastCommFromServer = process.hrtime();
+    send('pong');
+  } else {
+    myPos = JSON.parse(strMessage);
+    console.log(myPos);
+  }
+});
+
+updater.takeWhile(() => keepUpdating).onValue(() => {
+  var curTime = process.hrtime();
+  if(curTime[0] - lastCommFromServer[0] > 1) {
+    keepUpdating = false;
+    client.close();
+  }
+});
+
+send('give-port');
--- index.js
+++ index.js
@@ -1,36 +1,63 @@
 const PORT = 33333;
 const HOST = 'localhost';
 
+const B = require('baconjs');
 const dgram = require('dgram');
 const server = dgram.createSocket('udp4');
+const express = require('express');
+const app = express();
 const clientPort = 33334;
 const knownClients = {};
+
+const updates = B.repeatedly(16, ['update']);
 
 server.on('listening', () => {
   const address = server.address();
   console.log('UDP server listening on ' + address.address + ':' + address.port)
 });
 
-/*
-client.send(message, 0, message.length, PORT, HOST, (err, bytes) => {
-    if(err) throw err;
-    console.log('UDP message sent to ' + HOST + ':' + PORT);
-  });
-  */
 server.on('message', (message, remote) => {
-  //console.log(remote.address + ':' + remote.port + '-' + message);
-  console.log(knownClients);
-  console.log(message);
+  if(!knownClients[remote.port]) {
+    console.log(remote.port + ' connected');
+  }
   switch (message.toString()) {
     case 'give-port':
-      const msg = clientPort++;
-      console.log('remoteis',remote);
-      knownClients[clientPort] = {remote};
-
+      knownClients[remote.port] = {
+        lastComm: process.hrtime(),
+        position: {x: 0, y: 0}
+      };
       break;
+    case 'pong':
+      knownClients[remote.port].lastComm = process.hrtime()
     default:
-
   }
 });
 
+function tooLong(clientLastComm, currentTime) {
+  return (currentTime[0] - clientLastComm[0]) > 1
+}
+
 server.bind(PORT, HOST);
+
+updates.onValue(function () {
+  const knownKeys = Object.keys(knownClients);
+  const curTime = process.hrtime();
+  knownKeys.forEach(k => {
+    knownClients[k].position.x += 0.01;
+    knownClients[k].position.y += 0.01;
+    var message = new Buffer('ping');
+    var pos = new Buffer(JSON.stringify(knownClients[k].position));
+    server.send(message, 0, message.length, parseInt(k), 'localhost');
+    server.send(pos, 0, pos.length, parseInt(k), 'localhost');
+    if(tooLong(knownClients[k].lastComm, curTime)) {
+      console.log(k,'disconnected');
+      delete knownClients[k];
+    }
+  })
+});
+
+app.get('/', (req, res) => {
+  res.send(JSON.stringify(knownClients));
+});
+
+app.listen(8080);
